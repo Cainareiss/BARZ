f793ddcd0f97e98d71b47f9a519d94e6d590d81a
Cainã Reis <106223762+Cainareiss@users.noreply.github.com>
2023-02-28 11:46:56 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,18 +1,18 @@
 class Cliente:
 
-    def __init__(self, Nome, DT, TL):
+    def __init__(self, Nome='', DT='', TL=''):
         self.__Nome = Nome
         self.__DT = DT
         self.__TL = TL
 
-    def Nome(self):
-        self.__Nome = (str(input('Digite seu Nome:')))
+    def setNome(self):
+        self.__Nome = str(input('Digite seu Nome: '))
 
-    def DT(self):
-        self.__DT = int(input('Digite sua data de nascimento:'))
+    def setDT(self):
+        self.__DT = int(input('Digite sua data de nascimento: '))
 
-    def TL(self):
-        self.__TL = int(input('Digite seu telefone:'))
+    def setTL(self):
+        self.__TL = int(input('Digite seu telefone: '))
 
     def getNome(self):
         return self.__Nome
@@ -24,16 +24,11 @@ class Cliente:
         return self.__TL
 
     def showAllData(self):
-        return input((f"Dados da Pessoa:\nNome: {self.__Nome}\nData de Nascimento: {self.__DT}\nAltura: {self.__TL}"))
+        return (f"Dados da Pessoa:\nNome: {self.__Nome}\nData de Nascimento: {self.__DT}\nTelefone: {self.__TL}")
 
 
-cliente = Cliente
-print(Cliente)
-print(cliente.Nome)
-print(cliente.DT)
-print(cliente.TL)
-print(cliente.showAllData)
-# print(cliente.Dados())
-# print(cliente.DADOS('seus dados São:{}'.format()))
-#
-# #print('seus dados São:{}'.format(DADOS)
+cliente = Cliente()
+cliente.setNome()
+cliente.setDT()
+cliente.setTL()
+print(cliente.showAllData())
